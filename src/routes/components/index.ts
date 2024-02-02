08d9ca6ad7414f6b5e893a6f795613771e61c5ae
Meric Arda Eren <aeren@netas.com.tr>
2024-02-02 12:26:39 +0300

    File cleanup & change enable mocking node_env value

--- src/routes/components/index.ts
+++ src/routes/components/index.ts
@@ -1,4 +1,3 @@
 export { default as AppRoutes } from './AppRoutes'
-export { default as Layout } from './Layout'
 export { default as RequireAuth } from './RequireAuth'
 export { default as Unauthorized } from './Unauthorized'
